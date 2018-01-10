17b01e602177b9c255fff152b41cb44c23a44e8b
yifengxie <yifeng.macrodog@gmail.com>
2018-01-10 09:57:17 +0800

    添加文件

---
+++ Assets/Editor/SceneSerialize.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class SceneSerialize : Editor {
+    public string path = Application.dataPath + "/Resources/ScenseMap";
+
+    [MenuItem("XYFOptimizeTools/SaveScene")]
+     static void saveScene() {
+        GameObject scense = GameObject.Find("sceneMap");
+        XmlDocument xmlDocument= new XmlDocument();
+        XmlElement xmlElm= xmlDocument.CreateElement("Scene");
+        xmlElm.SetAttribute("Name", SceneManager.GetActiveScene().name);
+        if (scense!=null) {
+            for (int i = 0; i < scense.transform.childCount; i++) {
+                Transform child = scense.transform.GetChild(i);
+                getChildAndRecord(child);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 序列化
+    /// </summary>
+    /// <param name="parent"></param>
+    public static void  getChildAndRecord(Transform parent) {
+        
+    }
+}
