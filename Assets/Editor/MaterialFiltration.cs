022c9fd606b79efeb7cbb92687083fcd0542553a
yifengxie <yifeng.macrodog@gmail.com>
2018-01-10 13:35:01 +0800

    add material filtraion

--- Assets/Editor/MaterialFiltration.cs
+++ Assets/Editor/MaterialFiltration.cs
@@ -1,16 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
 
-public class MaterialFiltration : MonoBehaviour {
+public class MaterialFiltration : Editor
+{
+    [MenuItem(("XYFOptimizeTools/GenerateMaterial"))]
+    public static void filtration()
+    {
+        Dictionary<string, string> dicMaterial = new Dictionary<string, string>();
+        MeshRenderer[] meshRenders = Resources.FindObjectsOfTypeAll<MeshRenderer>();
+        string rootPath = Directory.GetCurrentDirectory();
+        for (int i = 0; i < meshRenders.Length; i++)
+        {
+            MeshRenderer meshRender = meshRenders[i];
+            EditorUtility.DisplayProgressBar("遍历mesh render中", meshRenders[i].name, (float)i / meshRenders.Length);
+            Material[] newMaterials = new Material[meshRender.sharedMaterials.Length];
+            for (int j = 0; j < newMaterials.Length; j++)
+            {
+                Material m = meshRender.sharedMaterials[j];
+                string mPath = AssetDatabase.GetAssetPath(m);
+                string fullPath = Path.Combine(rootPath, mPath);
+                if (!string.IsNullOrEmpty(mPath) && mPath.Contains("Assets/"))
+                {
+                    string text = File.ReadAllText(fullPath).Replace("m_Name: " + m.name, "");
+                    string change;
+                    if (!dicMaterial.TryGetValue(text, out change))
+                    {
+                        dicMaterial[text] = mPath;
+                        change = mPath;
+                    }
+                    else
+                    {
+                        if (mPath != change)
+                        {
+                            Debug.Log(mPath + " " + change);
+                            AssetDatabase.DeleteAsset(mPath);//删除重复的材质
+                        }
+                    }
+                    newMaterials[j] = AssetDatabase.LoadAssetAtPath<Material>(change);
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+                }
+            }
+            meshRender.sharedMaterials = newMaterials;
+        }
+        EditorUtility.ClearProgressBar();
+        EditorSceneManager.MarkAllScenesDirty();
+    }
 }
